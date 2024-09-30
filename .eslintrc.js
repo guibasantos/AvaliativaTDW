86790fe828d81dbd36f5fe4e3e3fada608799940
guibasantos <gwvsantos@hotmail.com>
2024-09-29 22:58:07 -0300

    ajustes 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      jest: true, // Adicione esta linha
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true // Adicione esta linha
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended' // Adicione esta linha se estiver usando o plugin jest
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:jest/recommended', // Adicione esta linha se estiver usando o plugin jest
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {
-      // suas regras personalizadas
-    },
-  };
-  
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    // suas regras personalizadas
+  }
+}
