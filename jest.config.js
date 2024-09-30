651be320526e540ac59c87e57beca7455d0866a6
guibasantos <gwvsantos@hotmail.com>
2024-09-29 23:02:40 -0300

    ajustes 4

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    testEnvironment: 'jsdom', // Para suportar o DOM nos testes
+    setupFilesAfterEnv: ['<rootDir>/setupTests.js'], // Caso tenha um arquivo de configuração adicional
+  };
+  
